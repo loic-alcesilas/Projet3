ff01ed60635a33302589b01c8b9b5fd65ec8d337
loic-alcesilas <loic.alcesilas@gmail.com>
2020-07-10 10:54:21 +0200

    Création diaporama

---
+++ assets/js/scripts.js
@@ -0,0 +1,55 @@
+// Variables d'ensemble
+let compteur = 0
+let timer
+let elements
+let slides
+let slideWidth
+
+
+window.onload = () => {
+    const diapo = document.querySelector(".diapo")
+    elements = document.querySelector(".elements")
+    slides = Array.from(elements.children)
+
+    //On calcule la largeur visible du diaporama
+    slideWidth = diapo.getBoundingClientRect().width
+
+    let next = document.querySelector("#nav-droite")
+    let prev = document.querySelector("#nav-gauche")
+    let pause = document.querySelector("#pause")
+    let play = document.querySelector("#play")
+
+    next.addEventListener("click", slideNext)
+    prev.addEventListener("click", slidePrev)
+    pause.addEventListener("click", slidePause)
+    play.addEventListener("click", slidePlay)
+
+    // Changer de slide tous les 5sec
+    timer = setInterval(slideNext, 5000)
+}
+
+function slideNext() {
+    compteur++
+    if(compteur == slides.length) {
+        compteur = 0
+    }
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
+}
+
+function slidePrev() {
+    compteur--
+    if (compteur < 0) {
+        compteur = slides.length - 1
+    }
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
+}
+
+function slidePause() {
+    clearInterval(timer)
+}
+
+function slidePlay() {
+    timer = setInterval(slideNext, 5000)
+}
