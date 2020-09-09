7aee71b814477011ee518de53eea7f3e8a3452ad
Tutt-J <contact@digistos.com>
2020-09-09 10:33:25 +0200

    remove double onclick

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,71 +1,68 @@
 // Variables d'ensemble
-let compteur = 0
-let timer
-let elements
-let slides
-let slideWidth
+let compteur = 0;
+let timer;
+let elements;
+let slides;
+let slideWidth;
 
 
 window.onload = () => {
     const diapo = document.querySelector(".diapo")
-    elements = document.querySelector(".elements")
-    slides = Array.from(elements.children)
+    elements = document.querySelector(".elements");
+    slides = Array.from(elements.children);
 
     //On calcule la largeur visible du diaporama
-    slideWidth = diapo.getBoundingClientRect().width
+    slideWidth = diapo.getBoundingClientRect().width;
 
-    let next = document.querySelector("#nav-droite")
-    let prev = document.querySelector("#nav-gauche")
-    let pause = document.querySelector("#pause")
-    let play = document.querySelector("#play")
+    let next = document.querySelector("#nav-droite");
+    let prev = document.querySelector("#nav-gauche");
+    let pause = document.querySelector("#pause");
+    let play = document.querySelector("#play");
 
-    next.addEventListener("click", slideNext)
-    prev.addEventListener("click", slidePrev)
-    pause.addEventListener("click", slidePause)
-    play.addEventListener("click", slidePlay)
+    next.addEventListener("click", slideNext);
+    prev.addEventListener("click", slidePrev);
+    pause.addEventListener("click", slidePause);
+    play.addEventListener("click", slidePlay);
 
     // Changer de slide tous les 5sec
-    timer = setInterval(slideNext, 5000)
+    timer = setInterval(slideNext, 2000);
 
     // RESPONSIVE 
     window.addEventListener("resize", () => {
-        slideWidth = diapo.getBoundingClientRect().width
-        slideNext()
+        slideWidth = diapo.getBoundingClientRect().width;
+        slideNext();
     })
 
     }
 
 function slideNext() {
-    compteur++
+    console.log(compteur);
+    compteur++;
     if(compteur == slides.length) {
-        compteur = 0
+        compteur = 0;
     }
-    let decal = -slideWidth * compteur
-    elements.style.transform = `translateX(${decal}px)`
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
 }
 
 function slidePrev() {
-    compteur--
-    if (compteur < 0) {
-        compteur = slides.length - 1
-    }
-    let decal = -slideWidth * compteur
-    elements.style.transform = `translateX(${decal}px)`
+    compteur--;
+    if (compteur < 0) {;
+        compteur = slides.length - 1;
+    };
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
 }
 
 function slidePause() {
-    //ajouter la classe d-none à #pause
-    // retirer la classe d-none à #play
     document.querySelector('#pause').classList.add("d-none");
     document.querySelector('#play').classList.remove("d-none");
     clearInterval(timer);
 }
 
 function slidePlay() {
-    //retirer la classe d-none à #pause
-    // ajouter la classe d-none à #play
-       document.querySelector('#play').classList.add("d-none");
+    document.querySelector('#play').classList.add("d-none");
     document.querySelector('#pause').classList.remove("d-none");
-    timer = setInterval(slideNext, 5000)
+    timer = setInterval(slideNext, 2000)
 }
 
