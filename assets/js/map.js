d60cecad771ba3949d0b44ebb0578f35f04c5fdc
loic-alcesilas <loic.alcesilas@gmail.com>
2020-09-23 19:00:41 +0200

    ajout du tableau html et debut js

--- assets/js/map.js
+++ assets/js/map.js
@@ -26,18 +26,36 @@ map.setCenter(lonLat, zoom);
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
+        var responses = JSON.parse(this.responseText);
+        responses.forEach(function (response) {
+            var markers = new OpenLayers.Layer.Markers("Markers");
+
+            var lonLat = new OpenLayers.LonLat(response['position']['lng'], response['position']['lat'])
+                .transform(
+                    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+                    map.getProjectionObject() // to Spherical Mercator Projection
+                );
+            map.addLayer(markers);
+
+            markers.addMarker(new OpenLayers.Marker(lonLat));
+
+            map.setCenter(lonLat, zoom);
+            console.log(response);
+        });
     }
 };
 request.open("GET", "https://api.jcdecaux.com/vls/v1/stations?contract=rouen&apiKey=fefa77128452c1aa0a3a63dd7a9f67bfcbcef4d5");
 request.send();
 
-ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=rouen&apiKey=fefa77128452c1aa0a3a63dd7a9f67bfcbcef4d5",
-    function (reponse) {
-        var rent = JSON.parse(reponse);
-        rent.forEach(function (station) {
-            var markers = new OpenLayers.Layer.Markers("Markers");
-        })
-    })
+//ajout du panneau d'information au click
+
+let stationinformations = document.getElementById('stationinfo');
+
+$(document).ready(function () {
+    $(L.marker).click(function () {
+        $('#stationinfo').removeClass('invisible');
+    });
+});
+
 
 
