93591c1ea3541d3b6ca93cedbf4fd03e813d4091
Tutt-J <contact@digistos.com>
2020-09-25 12:18:07 +0200

    session de mentorat

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,65 +1,37 @@
-//Recuperer toutes les stations de vélos via l'API JC DECAUX
+var map = L.map('maCarte').setView([49.4431,1.0993], 16);
 
-// carte et ajout du marqueurs 
-map = new OpenLayers.Map("maCarte");
-map.addLayer(new OpenLayers.Layer.OSM());
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG9pY29jIiwiYSI6ImNrZmkyNG13ajAycWgzMHFqanBvN3J5MTAifQ.AEuScT5GN9h-CXKSd69VFA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibG9pY29jIiwiYSI6ImNrZmkyNG13ajAycWgzMHFqanBvN3J5MTAifQ.AEuScT5GN9h-CXKSd69VFA'
+}).addTo(map);
 
-var lonLat = new OpenLayers.LonLat(1.0993, 49.4431)
-    .transform(
-        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-        map.getProjectionObject() // to Spherical Mercator Projection
-    );
-
-var zoom = 16;
-
-//Boucle sur l'objet ou le tableau renvoyé par JC Decaux et ajouter à chaque fois le marqueur
-
-var markers = new OpenLayers.Layer.Markers("Markers");
-map.addLayer(markers);
-
-markers.addMarker(new OpenLayers.Marker(lonLat));
-
-map.setCenter(lonLat, zoom);
-
-//ajout de l'api JcDecaux
+var onMarkerClick = function(address, status, nbBike){
+    alert("You clicked on marker: " + address);
+}
 
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var responses = JSON.parse(this.responseText);
         responses.forEach(function (response) {
-            var markers = new OpenLayers.Layer.Markers("Markers");
-
-            var lonLat = new OpenLayers.LonLat(response['position']['lng'], response['position']['lat'])
-                .transform(
-                    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-                    map.getProjectionObject() // to Spherical Mercator Projection
-                );
-            map.addLayer(markers);
-
-            markers.addMarker(new OpenLayers.Marker(lonLat));
-
-            map.setCenter(lonLat, zoom);
             console.log(response);
+            var marker = L.marker([response['position']['lat'], response['position']['lng']]);
+
+            //Handle click on marker
+            marker.on('click', onMarkerClick.bind(this, response.address,'c','d'));
+
+            marker.addTo(map);
+
+            marker = L.circleMarker([response['position']['lat'],response['position']['lng']], {fillColor: 'red'});
+            marker.addTo(map);
         });
     }
 };
 request.open("GET", "https://api.jcdecaux.com/vls/v1/stations?contract=rouen&apiKey=fefa77128452c1aa0a3a63dd7a9f67bfcbcef4d5");
 request.send();
-
-//ajout du panneau d'information au click
-
-let stationinformations = document.getElementById('stationinfo');
-
-$(document).ready(function () {
-
-    $(L.marker).click(function () {
-        function display() {
-            stationinformations.classList.replace("invisible", "visible");
-        }
-        display();
-    })
-})
-
 
 
