bed349339243a0f5293b5b1aa66236b8d78f3700
Tutt-J <contact@digistos.com>
2020-10-06 11:19:34 +0200

    session de mentorat

--- assets/js/map.js
+++ assets/js/map.js
@@ -10,15 +10,41 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(map);
 
 //essaie d'ajout des info sur la panneau 
-var onMarkerClick = function (address, name, available_bikes, status) {
+var onMarkerClick = function (infos) {
     document.getElementById('stationinfo').classList.replace("d-none", "d-initial");
     document.getElementById('map-container').classList.replace("col-12", "col-8");
-    document.getElementById('stationAddress').textContent = (address);
-    document.getElementById('stationName').textContent = (name);
-    document.getElementById('stationStatus').textContent = (status);
-    document.getElementById('stationBike').textContent = (available_bikes);
+    document.getElementById('stationAddress').textContent = (infos.address);
+    document.getElementById('stationName').textContent = (infos.name);
+    document.getElementById('stationStatus').textContent = (infos.status);
+    document.getElementById('stationBike').textContent = (infos.available_bikes+'/'+infos.bike_stands);
+    document.getElementById('stationStand').textContent = (infos.available_bike_stands +'/'+infos.bike_stands);
+    document.getElementById('number').value = infos.number;
 }
 
+document.getElementById("book").addEventListener("submit", submitForm);
+
+function  submitForm(){
+    var number= document.getElementById('number').value;
+    var firstname=document.getElementById("lastname").value;
+    var lastname=document.getElementById("firstname").value;
+
+    //si number ou firstname ou lastname est vide alors je mets une alert js avec un message
+    //Si pas de signature, une alerte
+    //Sinon si tout est bon alros on fait l'objet et on l'ajoute
+    var object = {
+        number: number,
+        name: firstname,
+        firstname: lastname,
+        'bookingtime':new Date()
+    }
+    localStorage.setItem("book", JSON.stringify(object));
+
+    //Ajouter en dessous du panneau al réservation
+}
+
+//number
+//nom
+//prenom
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
@@ -28,7 +54,8 @@ request.onreadystatechange = function () {
             var marker = L.marker([response['position']['lat'], response['position']['lng']]);
 
             //Handle click on marker
-            marker.on('click', onMarkerClick.bind(this, response.address, response.name, response.available_bikes, response.status));
+            marker.on('click', onMarkerClick.bind(this, response)
+            );
 
             marker.addTo(map);
 
